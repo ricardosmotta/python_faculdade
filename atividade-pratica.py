df70bd11a75791e4e2877b2dfab7e88d1f404961
Ricardo Motta <rmotta.qa@gmail.com>
2021-08-22 21:16:43 -0300

    update
    atualização do exercicio 1 do trabalho.
    agora existem 2 possibilidades.

--- atividade-pratica.py
+++ atividade-pratica.py
@@ -1,8 +1,29 @@
-nota = 0
+# nota = 0
+#
+# while nota >= 0 and nota <= 10:
+#     aluno = input('Nome do aluno: ')
+#     nota = float(input('Nota final: '))
+#     if (nota >= 0) and (nota <= 2.9):
+#         print('O aluno {} tirou nota {} e se enquadra no conceito E.'.format(aluno, nota))
+#     elif (nota >= 3) and (nota <= 4.9):
+#         print('O aluno {} tirou nota {} e se enquadra no conceito D.'.format(aluno, nota))
+#     elif (nota >= 5) and (nota <= 6.9):
+#         print('O aluno {} tirou nota {} e se enquadra no conceito C.'.format(aluno, nota))
+#     elif (nota >= 7) and (nota <= 8.9):
+#         print('O aluno {} tirou nota {} e se enquadra no conceito B.'.format(aluno, nota))
+#     elif (nota >= 9) and (nota <= 10):
+#         print('O aluno {} tirou nota {} e se enquadra no conceito A.'.format(aluno, nota))
+#     else:
+#         print('A nota digitada é inválida. Encerrando programa...')
 
-while nota >= 0 and nota <= 10:
+
+
+while True:
+    # input dos dados
     aluno = input('Nome do aluno: ')
     nota = float(input('Nota final: '))
+
+    # condicionais e saída de dados
     if (nota >= 0) and (nota <= 2.9):
         print('O aluno {} tirou nota {} e se enquadra no conceito E.'.format(aluno, nota))
     elif (nota >= 3) and (nota <= 4.9):
@@ -14,4 +35,6 @@ while nota >= 0 and nota <= 10:
     elif (nota >= 9) and (nota <= 10):
         print('O aluno {} tirou nota {} e se enquadra no conceito A.'.format(aluno, nota))
     else:
-        print('A nota digitada é inválida. Encerrando programa...')
+        if nota <= 0 or nota >= 10:
+            print('A nota digitada é inválida. Encerrando programa...')
+            break
